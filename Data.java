a2019f905c12df98fe28217cf762766a59773848
nurazlinamemey <nur_sari_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:53:08 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public static void main(String[] args) {
+     //tipe_data nama_var = value/isi var;
+     String nama = "memey";
+     int usia = 16;
+     String alamat  = "Jl.Danau Ranau Raya";
+     double kelas = 10.7;
+
+     System.out.println("Nama saya : "+ nama);
+     System.out.println("usia : " + usia + "tahun"); 
+     System.out.println("Alamat : " + alamat);
+     System.out.println("Kelas : " + kelas);
+
+    }
+}
