7f2505ca914121fa3fbd70bdeeec9c972cf36b45
nurazlinamemey <nur_sari_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 15:06:40 +0700

    Add files via upload

---
+++ SiswaTester.java
@@ -0,0 +1,74 @@
+//Driver Class
+public class SiswaTester{
+    public static void main(String[]aStrings){  
+        //object
+    //Class object=new constructor
+    SISWA nisa = new SISWA();
+    SISWA ria = new SISWA();
+    SISWA adi = new SISWA();
+    SISWA angga = new SISWA();
+    SISWA dion = new SISWA();
+    
+    angga.id = 5;
+    nisa.nama = "Annisa";
+    ria.ipk = 90;
+
+
+System.out.println("Nama "+adi.nama);
+System.out.println(nisa.nama);
+
+
+//Object
+//Class object = new constructor
+SISWA avelyn = new SISWA();
+SISWA keyza = new SISWA ();
+SISWA kalilah = new SISWA();
+SISWA leni = new SISWA();
+SISWA memey = new SISWA();
+ 
+
+avelyn.id = 4;
+avelyn.nama = "avelyn voleta wardani";
+avelyn.ipk = 90;
+
+keyza.id = 14;
+keyza.nama = "keyza nadine kusuma r";
+keyza.ipk = 90;
+
+kalilah.id = 12;
+kalilah.nama = "kalilah raihan rizky a";
+kalilah.ipk = 90;
+
+leni.id = 18;
+leni.nama = "leni sintiasari";
+leni.ipk = 90;
+
+memey.id = 31;
+memey.nama = "nur azlina mayla s";
+memey.ipk = 90;
+
+
+System.out.println("id" + avelyn.id);
+System.out.println("Nama : " + avelyn.nama);
+System.out.println("ipk : " + avelyn.ipk);
+
+System.out.println("id" + keyza.id);
+System.out.println("Nama : " + keyza.nama);
+System.out.println("ipk : " + keyza.ipk);
+
+System.out.println("id" + kalilah.id);
+System.out.println("Nama : " + kalilah.nama);
+System.out.println("ipk : " + kalilah.ipk);
+
+System.out.println("id" + leni);
+System.out.println("Nama : " + leni.nama);
+System.out.println("ipk : " + leni.ipk);
+
+System.out.println("id" + memey.id);
+System.out.println("Nama : " + memey.nama);
+System.out.println("ipk : " + memey.ipk);
+
+    }
+    
+
+}
